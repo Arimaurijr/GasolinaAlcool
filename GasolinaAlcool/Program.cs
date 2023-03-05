a6e9202c09d978a9d6378ffb1f0272dc7f753cf9
Ari Mauri <ari.mauri.jr@gmail.com>
2023-03-05 11:20:07 -0300

    Add project files.

---
+++ GasolinaAlcool/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double gasolina = 0.0, alcool = 0.0;
+        double rentabilidade = 0.0;
+
+        Console.WriteLine("Digite o preço da gasolina:");
+        gasolina = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Digite o preço do alcool:");
+        alcool = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        rentabilidade = alcool / gasolina;
+
+        if (rentabilidade > 0.72)
+        {
+            Console.WriteLine("Deve abastecer com GASOLINA !");
+        }
+        else
+        {
+            Console.WriteLine("Deve abastecer com ALCOOL !");
+        }
+    }
+}
